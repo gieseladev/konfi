afbd110f3b1266d7489fa9826f0eb79b3f6aa8be
siku2 <simon@siku2.io>
2019-07-01 16:44:11 +0200

    ignore requirements not met for file loaders

--- konfi/sources/__init__.py
+++ konfi/sources/__init__.py
@@ -2,5 +2,15 @@
 
 from .env import Env
 from .file import FileLoader, has_file_loader, register_file_loader
-from .toml import TOML
-from .yaml import YAML
+
+# TODO use dummy loaders which raise when they're being used
+
+try:
+    from .toml import TOML
+except ImportError:
+    pass
+
+try:
+    from .yaml import YAML
+except ImportError:
+    pass
